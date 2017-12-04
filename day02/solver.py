8a5194634d371e660e83f844fd1f1b9ab7ae90a7
Michael Patterson <mjpatter88@gmail.com>
2017-12-03 21:01:28 -0600

    Solve day 2 part 2.

--- day02/solver.py
+++ day02/solver.py
@@ -1,10 +1,17 @@
-checksum = 0
+from itertools import combinations
+
+checksum1 = 0
+checksum2 = 0
 
 with open("input.txt") as input_file:
     for line in input_file.readlines():
         nums = [int(n) for n in line.split()]
-        smallest = min(nums)
-        largest = max(nums)
-        checksum += (largest - smallest)
+        checksum1 += (max(nums) - min(nums))
+        for a, b in combinations(nums, 2):
+            div = max(a, b) / min(a, b)
+            if int(div) == div:
+                checksum2 += int(div)
 
-print(f'Part 1 answer: {checksum}')
+
+print(f'Part 1 answer: {checksum1}')
+print(f'Part 2 answer: {checksum2}')
