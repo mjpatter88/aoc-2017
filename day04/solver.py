5d25e1eee0eab2a2f93aa41babf00acd7d01e9f9
Michael Patterson <mjpatter88@gmail.com>
2017-12-07 23:03:48 -0600

    Add solution to part 2 of day 4.

--- day04/solver.py
+++ day04/solver.py
@@ -1,10 +1,23 @@
 
-valid = 0
+num_lines = 0
+invalid = 0
+invalid_2 = 0
 
 with open("input.txt") as input_file:
     for line in input_file.readlines():
         words = line.split()
-        if len(words) == len(set(words)):
-            valid += 1
+        num_lines += 1
+        sorted_words = ["".join(sorted(word)) for word in words]
 
+        if len(words) != len(set(words)):
+            invalid += 1
+            invalid_2 += 1
+
+        elif len(sorted_words) != len(set(sorted_words)):
+            invalid_2 += 1
+
+
+valid = num_lines - invalid
+valid_2 = num_lines - invalid_2
 print(f'Part 1 answer: {valid}')
+print(f'Part 2 answer: {valid_2}')
