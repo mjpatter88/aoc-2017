6f9bb2cfb83ea16ce75131ac8601546079594009
Michael Patterson <mjpatter88@gmail.com>
2017-12-17 01:06:33 -0600

    Solve part 1 of day 9.

---
+++ day09/solver.py
@@ -0,0 +1,54 @@
+
+class ProcessingMachine():
+    def __init__(self):
+        self.inside_garbage = False
+        self.ignore_next = False
+        self.line = []
+        self.level = 0
+        self.score = 0
+
+    def step(self, char):
+        if self.ignore_next:
+            self.ignore_next = False
+            return
+
+        if self.inside_garbage and char == '!':
+            self.ignore_next = True
+
+        if char == '<':
+            self.inside_garbage = True
+            return
+
+        if self.inside_garbage and char == '>':
+            self.inside_garbage = False
+            return
+
+        if not self.inside_garbage:
+            if char == '{':
+                self.level += 1
+            if char == '}':
+                self.score += self.level
+                self.level -= 1
+            self.line.append(char)
+
+    def get_line(self):
+        return ''.join(self.line)
+
+lines = []
+total_score = 0
+with open("input.txt") as input_file:
+    for line in input_file.readlines():
+        pm = ProcessingMachine()
+        line = line.strip()
+    #line = input_file.readline().strip()
+        for c in line:
+            pm.step(c)
+        print(pm.score)
+        total_score += pm.score
+        lines.append(pm.get_line())
+
+
+print(f'Part 1 answer: {total_score}')
+print(f'Part 2 answer: ')
+
+
