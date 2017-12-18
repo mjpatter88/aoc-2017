78eb6b7672a08094479b962ab852e2f866357fdb
Michael Patterson <mjpatter88@gmail.com>
2017-12-17 23:59:46 -0600

    Continuing working on part 1 of day 10.

--- day10/solver.py
+++ day10/solver.py
@@ -7,10 +7,17 @@ class CircularList:
 
     def __getitem__(self, given):
         if isinstance(given, slice):
-            #TODO: handle slice here
-            pass
+            return [self[x] for x in range(given.start, given.stop)]
         else:
-            return self.data[index % len(self.data)]
+            return self.data[given % len(self.data)]
+
+    def __setitem__(self, given, value):
+        if isinstance(given, slice):
+            start = given.start % len(self.data)
+            end = given.stop % len(self.data)
+            self.data[start:end] = value
+        else:
+            self.data[given % len(self.data)] = value
 
     def __repr__(self):
         return str(self.data)
@@ -27,11 +34,13 @@ cl = CircularList(test_list)
 with open("test_input.txt") as input_file:
     line = input_file.readline()
     lengths = [int(l) for l in line.split(',')]
+    print(cl)
     for length in lengths:
         cl.reverse_portion(current_position, length)
         current_position += length
         current_position += skip_size
         skip_size += 1
+        print(cl)
 
 print(cl)
 print(f'Part 1 answer: ')
