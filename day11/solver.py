e2c4956b0b0af6f44d67aa88eebecdfa63c724d3
Michael Patterson <mjpatter88@gmail.com>
2017-12-28 12:06:15 -0600

    Finish part 2 of day 11.

--- day11/solver.py
+++ day11/solver.py
@@ -24,13 +24,27 @@ def how_far_away(steps):
         e += e_inc
     return abs(n) + abs(e)
 
+def furthest(steps):
+    max_dist = 0
+    n, e = 0, 0
+    for step in steps:
+        n_inc, e_inc = evaluate(step)
+        n += n_inc
+        e += e_inc
+        new_dist = abs(n) + abs(e)
+        max_dist = max(max_dist, new_dist)
+    return max_dist
+
 with open("input.txt") as input_file:
     for line in input_file.readlines():
         line = line.strip()
         steps = line.split(",")
         print(steps)
         dist = how_far_away(steps)
+        furthest = furthest(steps)
         print(dist)
+        print(furthest)
 
 print(f'Part 1 answer: {dist}')
+print(f'Part 2 answer: {furthest}')
 
