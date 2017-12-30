e56314ab0efdf7fba0ee7546625905c553b33497
Michael Patterson <mpatterson@pillartechnology.com>
2017-12-30 00:17:38 -0600

    Solution for part 2 of day 12.

--- day12/solver.py
+++ day12/solver.py
@@ -3,6 +3,19 @@ from collections import deque
 
 group = defaultdict(set)
 
+def get_program(prog_id):
+    conns = set()
+    q = deque()
+    connections = group[prog_id]
+    q.extendleft(connections)
+
+    while len(q) > 0:
+        connection = q.popleft()
+        if connection not in conns:
+            conns.add(connection)
+            q.extendleft(group[connection])
+    return conns
+
 with open("input.txt") as input_file:
     for line in input_file.readlines():
         line = line.strip()
@@ -10,18 +23,16 @@ with open("input.txt") as input_file:
         right = right.split(", ")
         group[left] = right
 
-conns = set()
-q = deque()
-connections = group["0"]
-q.extendleft(connections)
-
-while len(q) > 0:
-    connection = q.popleft()
-    if connection not in conns:
-        conns.add(connection)
-        q.extendleft(group[connection])
-
-ans2 = 0
+first_id = "0"
+conns = get_program(first_id)
 print(f'Part 1 answer: {len(conns)}')
-print(f'Part 2 answer: {ans2}')
+num_groups = 1
+
+while len(conns) != len(group.keys()):
+    new_progs = group.keys() - conns
+    new_prog = new_progs.pop()
+    conns |= get_program(new_prog)
+    num_groups += 1
+
+print(f'Part 2 answer: {num_groups}')
 
