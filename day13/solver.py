457bcdb6d3ad661fb2d16c1cf88e36772ac834bf
Michael Patterson <mjpatter88@gmail.com>
2017-12-30 15:19:07 -0600

    Finish part 2 of day 13.

--- day13/solver.py
+++ day13/solver.py
@@ -4,15 +4,44 @@ class Layer():
     def __init__(self, r):
         self.range = r
         self.scanner_position = 0
-        positions = list(range(0, r)) + list(range(r-2, 0, -1))
-        self.cycler = cycle(positions)
-        self.step()
 
-    def step(self):
-        self.scanner_position = next(self.cycler)
+    def step(self, amount=1):
+        self.scanner_position += amount
 
     def does_catch(self):
-        return self.scanner_position == 0
+        cycle = (self.range - 1) * 2
+        return self.scanner_position % cycle == 0
+
+    def set_delay(self, amount):
+        self.scanner_position = amount
+
+def reset_layers():
+    for layer in layers.values():
+        layer.scanner_position = 0
+
+def get_total_severity():
+    total_severity = 0
+    position = -1 # Start prior to depth 0
+    while position < max_depth:
+        position += 1
+        layer = layers.get(position)
+        if layer and layer.does_catch():
+            total_severity += (layer.range * position)
+        for layer in layers.values():
+            layer.step()
+    return total_severity
+
+def is_clean_run():
+    position = 0 # Start prior to depth 0
+    while position <= max_depth:
+        layer = layers.get(position)
+        if layer:
+            layer.step(position)
+            if layer.does_catch():
+                return False
+        position += 1
+    return True
+
 
 layers = {}
 max_depth = 0
@@ -24,19 +53,19 @@ with open("input.txt") as input_file:
         layers[left] = Layer(right)
         max_depth = max(max_depth, left)
 
-total_severity = 0
-position = -1 # Start prior to depth 0
-while position < max_depth:
-    position += 1
-    layer = layers.get(position)
-    if layer and layer.does_catch():
-        total_severity += (layer.range * position)
-        print(f'caught: {position}')
+print(f'Part 1 answer: {get_total_severity()}')
+reset_layers()
+
+# TODO: make this better. Use math to calculate rather than brute force.
+delay = 0
+while True:
     for layer in layers.values():
-        layer.step()
+        layer.set_delay(delay)
+    if is_clean_run():
+        break
+    delay += 1
+    if delay % 100000 == 0:
+        print(delay)
 
-
-print(f'Part 1 answer: {total_severity}')
-
-# print(f'Part 2 answer:')
+print(f'Part 2 answer: {delay}')
 
