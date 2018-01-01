c51d1a521b561996adc04f2bca55d40d313bf8d1
Michael Patterson <mjpatter88@gmail.com>
2017-12-31 19:10:58 -0600

    Finish part 2 of day 14.

--- day14/solver.py
+++ day14/solver.py
@@ -45,11 +45,65 @@ with open("input.txt", "r") as input_file:
         hashes.append(knot_hash)
         print(str(x) + '\r', end='')
 
+binary_map = []
 num_ones = 0
 for h in hashes:
+    row = []
     for c in h:
         binary = bin(int(c, 16))[2:] # We just want the 1s and 0s
+        binary = binary.zfill(4)
+        row.extend(binary)
         num_ones += binary.count('1')
+    binary_map.append(row)
 
 print(f'Part 1 answer: {num_ones}')
 
+used_marker = 'x'
+region_map = []
+for row in binary_map:
+    new_row = ['x' if r =='1' else 'o' for r in row]
+    region_map.append(new_row)
+
+def map_region(row_index, col_index, region_map, region_num):
+    region_map[row_index][col_index] = str(region_num)
+
+    # left
+    if col_index > 0 and region_map[row_index][col_index-1] == used_marker:
+        map_region(row_index, col_index-1, region_map, region_num)
+
+    # right
+    if col_index < 127 and region_map[row_index][col_index+1] == used_marker:
+        map_region(row_index, col_index+1, region_map, region_num)
+
+    # top
+    if row_index > 0 and region_map[row_index-1][col_index] == used_marker:
+        map_region(row_index-1, col_index, region_map, region_num)
+
+    # bottom
+    if row_index < 127 and region_map[row_index+1][col_index] == used_marker:
+        map_region(row_index+1, col_index, region_map, region_num)
+
+# Walk through row by row. If an x is encountered, replace it with the currrent region number and trace all adjacent xs
+# Then increment region number and restart
+current_region_num = 1
+start_over = False
+while True:
+    for row_i, row in enumerate(region_map):
+        for col_i, c in enumerate(row):
+            if c == used_marker:
+                map_region(row_i, col_i, region_map, current_region_num)
+                current_region_num += 1
+                start_over = True
+                break
+        if start_over:
+            break
+    if not start_over:
+        break
+    if start_over:
+        start_over = False
+# TODO: fix this terrible start_over logic
+
+# for row in region_map:
+#    print("".join(r for r in row))
+
+print(f'Part 2 answer: {current_region_num-1}')
