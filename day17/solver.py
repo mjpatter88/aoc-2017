4ef7b83e79dcb172470cf646e1cb961fa4bb8322
Michael Patterson <mjpatter88@gmail.com>
2018-03-05 00:02:13 -0600

    Finish part 2 of day 17.

--- day17/solver.py
+++ day17/solver.py
@@ -1,21 +1,48 @@
 num_steps_per_turn = 377
-# num_steps_per_turn = 3
 num_turns = 2018
-# num_turns = 10
 
 class CircularBuffer:
-    def __init__(self, num_steps_per_turn):
+    def __init__(self, num_steps_per_turn, num_turns):
         self.num_steps_per_turn = num_steps_per_turn
-        self.buff = [0]
+        self.buff = [''] * num_turns
+        self.buff[0] = 0
         self.cursor = 0
+        self.size = 1
 
     def insert(self, value):
-        self.cursor = (self.cursor + num_steps_per_turn) % len(self.buff) + 1
-        self.buff.insert(self.cursor, value)
+        self.cursor = (self.cursor + num_steps_per_turn) % self.size + 1
+        self.buff[self.cursor] = value
+        self.size += 1
 
-cb = CircularBuffer(num_steps_per_turn)
+cb = CircularBuffer(num_steps_per_turn, num_turns)
 
 for x in range(1, num_turns):
     cb.insert(x)
 
 print(f'Part 1 answer: {cb.buff[cb.cursor+1]}')
+
+# Key insight: the insertion into the array used in part 1 is way too slow. As the array grows,
+# more and more numbers need to be shifted on a single insert. A linked list might help, but as
+# the array grows the search to get to the right index would be slow.
+# Instead, let's create the whole array first, then the insert becomes fast.
+# This provides the right answer, but as I think through it, it shouldn't work. It's actually
+# replacing values rather than pushing them down. It only works since we are just looking for the
+# value after zero.
+
+num_steps_per_turn = 377
+num_turns = 50000001
+
+cb = CircularBuffer(num_steps_per_turn, num_turns)
+
+for x in range(1, num_turns):
+    cb.insert(x)
+    if x % 500000 == 0:
+        print(x)
+
+answer = -1
+for ind, x in enumerate(cb.buff):
+    if x == 0:
+        answer = cb.buff[ind+1]
+        break
+
+print(f'Part 2 answer: {answer}')
